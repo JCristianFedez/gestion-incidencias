5fd7385a67fba7ba5a5d83ab6c7ee471cf881120
JCristian <73657780+JCristianFedez@users.noreply.github.com>
2021-03-16 19:27:52 +0100

    5 Seeders

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $table->id();
+        // $table->string('name');
+        // $table->string('email')->unique();
+        // $table->timestamp('email_verified_at')->nullable();
+        // $table->string('password');
+        // $table->smallInteger("role"); //0: Admin | 1: Support | 2:Client
+        // $table->rememberToken();
+        // $table->timestamps();
+
+        User::create([
+            "name" => "admin",
+            "email" => "admin@gmail.com",
+            "password" => bcrypt("123"),
+            "role" => 0
+        ]);
+
+        User::create([
+            "name" => "support",
+            "email" => "support@gmail.com",
+            "password" => bcrypt("123"),
+            "role" => 1
+        ]);
+
+        User::create([
+            "name" => "client",
+            "email" => "client@gmail.com",
+            "password" => bcrypt("123"),
+            "role" => 2
+        ]);
+
+    }
+}
