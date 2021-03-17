3c8474a11f5aabc623cede24d23d71e480bebfa2
JCristian <73657780+JCristianFedez@users.noreply.github.com>
2021-03-17 16:26:23 +0100

    12 Items del menu con class active

--- resources/views/layouts/includes/menu.blade.php
+++ resources/views/layouts/includes/menu.blade.php
@@ -5,31 +5,33 @@
         <ul class="nav nav-pills">
             @if (auth()->check())
                 <li class="nav-item">
-                    <a href="/home" class="nav-link">Dashboard</a>
+                    <a href="/home" class="nav-link @if(request()->is("home"))active @endif">Dashboard</a>
                 </li>
                 <li class="nav-item">
-                    <a href="/ver" class="nav-link">Ver incidencias</a>
+                    <a href="/ver" class="nav-link @if(request()->is("ver"))active @endif">Ver incidencias</a>
                 </li>
                 <li class="nav-item">
-                    <a href="/reportar" class="nav-link">Reportar incidencia</a>
+                    <a href="/reportar" class="nav-link @if(request()->is("reportar"))active @endif">Reportar incidencia</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Administracion</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="/usuarios">Usuarios</a>
-                        <a class="dropdown-item" href="/proyectos">Proyecto</a>
-                        <a class="dropdown-item" href="/config">Configuracion</a>
-                    </div>
-                </li>
+                @if (auth()->user()->is_admin)
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Administracion</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="/usuarios">Usuarios</a>
+                            <a class="dropdown-item" href="/proyectos">Proyecto</a>
+                            <a class="dropdown-item" href="/config">Configuracion</a>
+                        </div>
+                    </li>
+                @endif
             @else
                 <li class="nav-item">
-                    <a href="/" class="nav-link">Bienvenido</a>
+                    <a href="/" class="nav-link @if(request()->is("/"))active @endif">Bienvenido</a>
                 </li>
                 <li class="nav-item">
-                    <a href="/instrucciones" class="nav-link">Instrucciones</a>
+                    <a href="/instrucciones" class="nav-link @if(request()->is("instrucciones"))active @endif">Instrucciones</a>
                 </li>
                 <li class="nav-item">
-                    <a href="/acerca-de" class="nav-link">Créditos</a>
+                    <a href="/acerca-de" class="nav-link @if(request()->is("acerca-de"))active @endif">Créditos</a>
                 </li>
             @endif
         </ul>
