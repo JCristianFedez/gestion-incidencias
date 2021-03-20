a102abd1c38d7d3b9f25386d9b8c6c07b9242205
JCristian <73657780+JCristianFedez@users.noreply.github.com>
2021-03-20 20:43:54 +0100

    Agregado primer toast de notificacion

--- public/js/main.js
+++ public/js/main.js
@@ -6,3 +6,11 @@ function onNewProjectSelected(){
     let project_id = $(this).val();
     location.href = `/seleccionar/proyecto/${project_id}`;
 }
+
+$(document).ready(function(){
+    $(".toast").toast();
+    
+    setTimeout(function() {
+            $(".toast").fadeOut(1500);
+    },3000);
+});
--- resources/views/incidents/show.blade.php
+++ resources/views/incidents/show.blade.php
@@ -12,9 +12,19 @@
         @endif
 
         @if (session("notification"))
-        <div class="alert alert-success">
-            {{ session("notification") }}
-        </div>
+            <div class="toast fixed-top fade show ml-auto" role="alert" aria-live="assertive" aria-atomic="true">
+                <div class="toast-header">
+                    <i class="fas fa-bell text-warning mr-3"></i>
+                    <strong class="mr-auto">Atencion</strong>
+                    <small>Ahora mismo</small>
+                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                        <span aria-hidden="true" class="text-black-50">&times;</span>
+                    </button>
+                </div>
+                <div class="toast-body">
+                    {{ session("notification") }}
+                </div>
+            </div>
         @endif
 
         @if (count($errors) > 0)
