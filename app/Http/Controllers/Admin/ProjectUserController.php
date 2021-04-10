610c01194fd5538ae6f4b1d312915b024fefa712
JCristian <73657780+JCristianFedez@users.noreply.github.com>
2021-04-10 20:54:16 +0200

    M18 Areglado: Cuando se elimina una relacion se puede luego volver a crear

--- app/Http/Controllers/Admin/ProjectUserController.php
+++ app/Http/Controllers/Admin/ProjectUserController.php
@@ -36,7 +36,12 @@ class ProjectUserController extends Controller
             ->where("user_id",$request->user_id)->first();
 
         if($project_user){
-            return back()->with("notification", "El usuario ya pertenece a este proyecto.");
+            if($project_user->deleted_at == null){
+                return back()->with("notification", "El usuario ya pertenece a este proyecto.");
+            }else{
+                $project_user->restore();
+                return back()->with("notification", "Usuario agregado al proyecto.");
+            }
         }
 
         $project_user = new ProjectUser();
@@ -45,7 +50,7 @@ class ProjectUserController extends Controller
         $project_user->level_id = $request->level_id;
         $project_user->save();
 
-        return back();
+        return back()->with("notification", "Usuario agregado al proyecto.");
     }
 
     public function update(Request $request){
