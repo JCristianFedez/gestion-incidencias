0a6e99da58233fb91dceca062e3fc6c2e3c9bd4d
JCristian <73657780+JCristianFedez@users.noreply.github.com>
2021-04-24 09:02:12 +0200

    Primera version instrucciones

---
+++ resources/views/instructions/includes/vertical-index.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-light bg-light">
+        <span class="navbar-brand text-center mx-auto">Indice</span>
+        <nav class="nav nav-pills flex-column" id="scrollspy-demo">
+                <a class="nav-link mb-1" href="#about">About this demo</a>
+                <a class="nav-link" href="#picture">Pictures</a>
+                <nav class="nav nav-pills flex-column">
+                        <a class="nav-link ml-4 my-1" href="#picture-1">Picture 1</a>
+                        <nav class="nav nav-pills flex-column">
+                                <a class="nav-link ml-5" href="#picture-1-1">Picture 1-1</a>
+                                <a class="nav-link ml-5" href="#picture-1-2">Picture 1-2</a>
+                                <a class="nav-link ml-5" href="#picture-1-3">Picture 1-3</a>
+                        </nav>
+                        <a class="nav-link ml-4 my-1" href="#picture-2">Picture 2</a>
+                        <nav class="nav nav-pills flex-column">
+                                <a class="nav-link ml-5" href="#picture-2-1">Picture 2-1</a>
+                                <a class="nav-link ml-5" href="#picture-2-2">Picture 2-2</a>
+                                <a class="nav-link ml-5" href="#picture-2-3">Picture 2-3</a>
+                        </nav>
+                </nav>
+                <a class="nav-link mb-1" href="#slide">Slides</a>
+                <nav class="nav nav-pills flex-column">
+                        <a class="nav-link ml-4" href="#slide-1">Slide 1</a>
+                        <a class="nav-link ml-4" href="#slide-2">Slide 2</a>
+                        <a class="nav-link ml-4" href="#slide-3">Slide 3</a>
+                </nav>
+        </nav>
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -113,7 +113,7 @@
 
                 {{-- Contenido --}}
                 <main class="container-fluid">
-                    <div class="page-content px-5 pt-3 pb-5" id="content">
+                    <div class="page-content px-3 pt-3 pb-5" id="content">
                         <h1 class="h2 border-bottom mb-3">@yield('tituloPagina')</h1>
                         @yield('content')
                     </div>
